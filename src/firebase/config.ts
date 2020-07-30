54914cbfc3a92f0724ac870a921e0993ad8a794a
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-07-30 19:12:25 -0300

    Add firebase config file

---
+++ src/firebase/config.ts
@@ -0,0 +1,14 @@
+/*var firebaseConfig = {
+  apiKey: "AIzaSyBkEdjuBZSMnXXz2ioUuqJDR0ANYGaTjg8",
+  authDomain: "blazing-firegram.firebaseapp.com",
+  databaseURL: "https://blazing-firegram.firebaseio.com",
+  projectId: "blazing-firegram",
+  storageBucket: "blazing-firegram.appspot.com",
+  messagingSenderId: "366196135247",
+  appId: "1:366196135247:web:99330d7c06b968ccaca13a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+*/
+
+export default {}
