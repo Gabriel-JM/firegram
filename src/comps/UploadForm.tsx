e7c5669ad5a15dbcc98654f8f8511b9ab6e1413b
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-07-30 21:17:52 -0300

    Make the upload message when is a valid file and when isn't a valid file

--- src/comps/UploadForm.tsx
+++ src/comps/UploadForm.tsx
@@ -1,16 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 const UploadForm: FC = () => {
+  const [file, setFile] = useState<File>({} as File)
+  const [error, setError] = useState<string>()
+
+  const allowedTypes = ['image/png', 'image/jpeg']
 
   const changeImgHandler = (event: React.ChangeEvent) => {
     const target = event.target as HTMLInputElement
-    let selected = target.files && target?.files[0]
-    console.log(selected)
+    const selected = target.files && target?.files[0]
+    
+    if (selected && allowedTypes.includes(selected.type)) {
+      setFile(selected)
+      setError('')
+    } else {
+      setFile({} as File)
+      setError('Please select an image file (png or jpeg)')
+    }
   }
 
   return (
     <form>
       <input type="file" onChange={changeImgHandler} />
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
     </form>
   )
 }
